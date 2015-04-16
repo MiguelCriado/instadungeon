a015e468d71cf35aed93ac891dfd633146900b68
Miguel Ángel Criado Romero <macro.masek@gmail.com>
2015-04-16 23:41:09 +0200

    * Some RectangleInt's methods fixed and properly tested with Unity Test Tools.

--- Assets/Scripts/LayoutZone.cs
+++ Assets/Scripts/LayoutZone.cs
@@ -9,35 +9,6 @@ public class LayoutZone {
 
     private Dictionary<Vector2Int, Tile> tiles;
 
-    public List<Vector2Int> ContactArea(LayoutZone other)
-    {
-        List<Vector2Int> result = new List<Vector2Int>();
-        int xMin = 0, xMax = 0, yMin = 0, yMax = 0;
-        if (other.bounds.position.y > bounds.y + bounds.height) {               // other is on top 
-            yMin = yMax = bounds.y + bounds.height;
-            xMin = Mathf.Max(bounds.x, other.bounds.x);
-            xMax = Mathf.Min(bounds.x, other.bounds.x);
-        } else if (other.bounds.position.x > bounds.x + bounds.width){          // other is right
-            xMin = xMax = bounds.x + bounds.width;
-            yMin = Mathf.Max(bounds.y, other.bounds.y);
-            yMax = Mathf.Min(bounds.y, other.bounds.y);
-        } else if (other.bounds.position.y + other.bounds.height < bounds.y) {  // other is down
-            yMin = yMax = bounds.y;
-            xMin = Mathf.Max(bounds.x, other.bounds.x);
-            xMax = Mathf.Min(bounds.x, other.bounds.x);
-        } else if (other.bounds.position.x + other.bounds.width < bounds.x) {   // other is left
-            xMin = xMax = bounds.x;
-            yMin = Mathf.Max(bounds.y, other.bounds.y);
-            yMax = Mathf.Min(bounds.y, other.bounds.y);
-        }
-        for (int i = xMin; i <= xMax; i++) {
-            for (int j = yMin; j <= yMax; j++) {
-                result.Add(new Vector2Int(i, j));
-            }
-        }
-        return result;
-    }
-
     public Vector2Int ContactPoint(Vector2Int layoutZonePoint) {
         return new Vector2Int(0,0);
     }
--- Assets/Scripts/Math/RectangleInt.cs
+++ Assets/Scripts/Math/RectangleInt.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class RectangleInt : MonoBehaviour {
+public class RectangleInt {
 
     public Vector2Int position
     {
@@ -48,5 +49,53 @@ public class RectangleInt : MonoBehaviour {
             && x + width > other.x
             && y < other.y + other.height
             && y + height > other.y;
+    }
+
+    public bool IsAdjacent(RectangleInt other)
+    {
+        return ((y + height == other.y || y == other.y + other.height) && (x < other.x + other.width || x + width > other.x))
+            || ((x + width == other.x || x == other.x + other.width) && (y < other.y + other.height || y + height > other.y));
+    }
+
+    public List<Vector2Int> ContactArea(RectangleInt other)
+    {
+        List<Vector2Int> result = new List<Vector2Int>();
+        int xMin = 0, xMax = 0, yMin = 0, yMax = 0;
+        if (IsAdjacent(other))
+        {
+            if (other.y >= y + height)
+            { // other is on top 
+                yMin = yMax = y + height - 1;
+                xMin = Mathf.Max(x, other.x);
+                xMax = Mathf.Min(x + width - 1, other.x + other.width - 1);
+            }
+            else if (other.x >= x + width)
+            { // other is right
+                xMin = xMax = x + width - 1;
+                yMin = Mathf.Max(y, other.y);
+                yMax = Mathf.Min(y + height - 1, other.y + other.height - 1);
+            }
+            else if (other.y + other.height <= y)
+            { // other is down
+                yMin = yMax = y;
+                xMin = Mathf.Max(x, other.x);
+                xMax = Mathf.Min(x + width - 1, other.x + other.width - 1);
+            }
+            else if (other.x + other.width <= x)
+            { // other is left
+                xMin = xMax = x;
+                yMin = Mathf.Max(y, other.y);
+                yMax = Mathf.Min(y + height - 1, other.y + other.height - 1);
+            }
+            for (int i = xMin; i <= xMax; i++)
+            {
+                for (int j = yMin; j <= yMax; j++)
+                {
+                    result.Add(new Vector2Int(i, j));
+                }
+            }
+        }
+        
+        return result;
     }
 }
