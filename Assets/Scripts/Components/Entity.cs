19415c8833772ea74b13c3b671232cf6d63a3214
Miguel Ángel Criado Romero <macro.masek@gmail.com>
2016-12-03 13:58:55 +0100

    * Added Entity class to represent all actors in the game.

---
+++ Assets/Scripts/Components/Entity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace InstaDungeon
+{
+	public class Entity : MonoBehaviour
+	{
+		public Blackboard Blackboard { get { return blackboard; } }
+
+		private Blackboard blackboard;
+
+		void Awake()
+		{
+			blackboard = new Blackboard();
+		}
+	}
+}
