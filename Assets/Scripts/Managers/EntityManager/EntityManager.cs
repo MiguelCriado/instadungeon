08ae35ee41f425f8a5a829a57623958c8509d12b
Miguel Ángel Criado Romero <macro.masek@gmail.com>
2016-12-04 13:37:23 +0100

    * Added Promises package.

---
+++ Assets/Scripts/Managers/EntityManager/EntityManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UniqueId = System.Int32;
+
+namespace InstaDungeon
+{
+	public class EntityManager : MonoBehaviour
+	{
+		private Dictionary<UniqueId, Entity> dynamicEntities;
+
+		void Awake()
+		{
+			dynamicEntities = new Dictionary<UniqueId, Entity>();
+		}
+
+		public Entity Spawn(string entityType)
+		{
+			Entity result = null;
+
+			return result;
+		}
+	}
+}
