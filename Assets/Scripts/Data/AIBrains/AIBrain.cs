eeae47c3cf69dfd44c628bffc19eb0ecd7765820
Miguel Ángel Criado Romero <macro.masek@gmail.com>
2017-05-06 13:03:00 +0200

    Add base enemy AI classes

---
+++ Assets/Scripts/Data/AIBrains/AIBrain.cs
@@ -0,0 +1,11 @@
+﻿using AI.BehaviorTrees;
+using InstaDungeon.Components;
+using UnityEngine;
+
+namespace InstaDungeon.AI
+{
+	public abstract class AIBrain : ScriptableObject
+	{
+		public abstract void Think(Entity context, Actor actor, Blackboard blackboard);
+	}
+}
---
+++ Assets/Scripts/Data/AIBrains/ChaserBrain.cs
@@ -0,0 +1,15 @@
+﻿using AI.BehaviorTrees;
+using InstaDungeon.Components;
+using UnityEngine;
+
+namespace InstaDungeon.AI
+{
+	[CreateAssetMenu(menuName = "InstaDungeon/AI Brains/ChaserBrain", fileName = "new ChaserBrain", order = 1000)]
+	public class ChaserBrain : AIBrain
+	{
+		public override void Think(Entity context, Actor actor, Blackboard blackboard)
+		{
+			// TODO: chase the player
+		}
+	}
+}
