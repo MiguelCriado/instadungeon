2ce6543ddca6d2fbfcfd0e1b2fbf2ee7a55a7161
Miguel Ángel Criado Romero <macro.masek@gmail.com>
2017-05-07 22:19:59 +0200

    Add Health component and Weapon data class

---
+++ Assets/Scripts/Components/Health.cs
@@ -0,0 +1,70 @@
+﻿using InstaDungeon.Models;
+using UnityEngine;
+
+namespace InstaDungeon.Components
+{
+	public class Health : MonoBehaviour
+	{
+		public int MaxHealth { get { return maxHealth; } }
+		public int CurrentHealth { get { return currentHealth; } }
+
+		[SerializeField] private int maxHealth;
+		[SerializeField] private int currentHealth;
+
+		private Inventory inventory;
+
+		private void Reset()
+		{
+			maxHealth = 10;
+			currentHealth = maxHealth;
+		}
+
+		private void OnValidate()
+		{
+			currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+		}
+
+		private void Awake()
+		{
+			Initialize();
+		}
+
+		public void Initialize()
+		{
+			currentHealth = maxHealth;
+		}
+
+		public int Attack(Weapon weapon)
+		{
+			int weaponDamage = weapon.GetRandomDamage();
+			int totalDamage = weaponDamage - CalculateDefense();
+			int damageDealt = Mathf.Max(0, totalDamage);
+
+			currentHealth = Mathf.Max(0, currentHealth - damageDealt);
+
+			if (currentHealth <= 0)
+			{
+				// TODO: notify entity death
+			}
+
+			return totalDamage;
+		}
+
+		public int Heal(int amount)
+		{
+			int effectiveHeal = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+			currentHealth += effectiveHeal;
+
+			return effectiveHeal;
+		}
+
+		private int CalculateDefense()
+		{
+			int result = 0;
+
+			// TODO fetch the inventory for defense equipment
+
+			return result;
+		}
+	}
+}
