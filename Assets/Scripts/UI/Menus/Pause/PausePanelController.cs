a937fcc2839f260eb109bf4c995de9090d2ecb6b
Miguel Ángel Criado Romero <macro.masek@gmail.com>
2017-08-18 22:23:04 +0200

    Add pause menu

---
+++ Assets/Scripts/UI/Menus/Pause/PausePanelController.cs
@@ -0,0 +1,123 @@
+﻿using DG.Tweening;
+using InstaDungeon.Events;
+using UnityEngine;
+
+namespace InstaDungeon.UI
+{
+	public class PausePanelController : MonoBehaviour
+	{
+		[Header("References")]
+		[SerializeField] private RectTransform content;
+		[SerializeField] private RectTransform optionSelector;
+		[SerializeField] private MenuOption continueOption;
+		[SerializeField] private MenuOption retryOption;
+		[SerializeField] private MenuOption mainMenuOption;
+		[Header("Settings")]
+		[SerializeField] private KeyCode pauseKey;
+
+		private MenuOption currentSelectedOption;
+
+		private void Reset()
+		{
+			pauseKey = KeyCode.Escape;
+		}
+
+		private void Awake()
+		{
+			continueOption.OnOptionSelected.AddListener(() => 
+			{
+				MoveSelector(continueOption.GetComponent<RectTransform>());
+				SetSelectedOption(continueOption);
+			});
+
+			continueOption.OnOptionPressed.AddListener(() => 
+			{
+				GameManager.SetState(GameState.Running);
+			});
+
+			retryOption.OnOptionSelected.AddListener(() =>
+			{
+				MoveSelector(retryOption.GetComponent<RectTransform>());
+				SetSelectedOption(retryOption);
+			});
+
+			retryOption.OnOptionPressed.AddListener(() =>
+			{
+				GameManager.ResetGame();
+			});
+
+			mainMenuOption.OnOptionSelected.AddListener(() =>
+			{
+				MoveSelector(mainMenuOption.GetComponent<RectTransform>());
+				SetSelectedOption(mainMenuOption);
+			});
+
+			mainMenuOption.OnOptionPressed.AddListener(() =>
+			{
+				// TODO
+			});
+		}
+
+		private void Start()
+		{
+			GameManager.Events.AddListener(OnGameStateChange, GameStateChangeEvent.EVENT_TYPE);
+			content.gameObject.SetActive(false);
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(pauseKey) && GameManager.GameState == GameState.Running)
+			{
+				GameManager.SetState(GameState.Paused);
+			}
+		}
+
+		public void BackgroundSelected()
+		{
+			if (currentSelectedOption != null)
+			{
+				currentSelectedOption.Select();
+			}
+		}
+
+		private void OnGameStateChange(IEventData eventData)
+		{
+			GameStateChangeEvent stateEvent = eventData as GameStateChangeEvent;
+
+			if (stateEvent.State == GameState.Paused)
+			{
+				OnPause();
+			}
+			else
+			{
+				HideMenu();
+			}
+		}
+
+		private void OnPause()
+		{
+			content.gameObject.SetActive(true);
+			Canvas.ForceUpdateCanvases();
+			continueOption.Select();
+		}
+
+		private void HideMenu()
+		{
+			if (content.gameObject.activeInHierarchy)
+			{
+				content.gameObject.SetActive(false);
+			}
+		}
+
+		private void MoveSelector(RectTransform transform)
+		{
+			DOTween.Kill(optionSelector);
+			optionSelector.DOMove(transform.position, 0.15f).SetEase(Ease.InOutSine);
+		}
+
+		private void SetSelectedOption(MenuOption menuOption)
+		{
+			currentSelectedOption = menuOption;
+		}
+	}
+}
