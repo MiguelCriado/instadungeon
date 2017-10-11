c621fdf5cc30d3fc4cdb185a51e6ec4ba9b15643
Miguel Ángel Criado Romero <macro.masek@gmail.com>
2017-10-11 21:57:00 +0200

    Add Small Health Potion

---
+++ Assets/Scripts/Components/Items/HealthPotion.cs
@@ -0,0 +1,13 @@
+﻿using InstaDungeon.Configuration;
+using UnityEngine;
+
+namespace InstaDungeon.Models
+{
+	public class HealthPotion : Item
+	{
+		public override ItemInfo ItemInfo { get { return healthPotionInfo; } }
+		public HealthPotionInfo HealthPotionInfo { get { return healthPotionInfo; } }
+
+		[SerializeField] private HealthPotionInfo healthPotionInfo;
+	}
+}
---
+++ Assets/Scripts/Data/Configuration/Items/HealthPotionInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace InstaDungeon.Configuration
+{
+	[CreateAssetMenu(menuName = "InstaDungeon/Configuration/Items/HealthPotionInfo", fileName = "new HealthPotion", order = 1000)]
+	public class HealthPotionInfo : ItemInfo
+	{
+		public int HealthRestored { get { return healthRestored; } }
+
+		[SerializeField] int healthRestored;
+	}
+}
